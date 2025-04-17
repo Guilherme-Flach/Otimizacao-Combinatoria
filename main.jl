b539a7ab8c12f3ea09d58f3313e126544fe21d40
Guilherme Flach <gui.ismael.flach@gmail.com>
2025-04-16 23:47:55 -0300

    Command line reading for args

--- main.jl
+++ main.jl
@@ -5,40 +5,50 @@ include("src/localSearch.jl")
 
 using Random
 
-# Variables to be extracted as command line params
-randomSeed = 7
-filePath = "instances/01.txt"
-iterationsNum = 100
-
-Random.seed!(randomSeed)
-
-instance::Instance = reader(filePath)
-
-println("Number of prisioners: $(instance.n)")
-println("Number of restrictions: $(instance.m)")
-println("Restrictions:")
-display(instance.alliances)
-
-
-# Initialize a trivial valid solution (each prisioner in its own prision)
-function generateTrivialSolution()
-    solution = Vector{Prision}()
-    for i in 1:instance.n
-        push!(solution, [i])
+function main()
+    # Variables to be extracted as command line params
+    if (size(ARGS)[1] !== 3)
+        println("Error reading args. Usage: `main.jl filepath iterations randomSeed`")
+        return
     end
-    return Solution(solution, instance.n)
+
+    filePath = ARGS[1]
+    iterationsNum = parse(Int, ARGS[2])
+    randomSeed = parse(Int, ARGS[3])
+
+
+    Random.seed!(randomSeed)
+
+    instance::Instance = reader(filePath)
+
+    println("Number of prisioners: $(instance.n)")
+    println("Number of restrictions: $(instance.m)")
+    println("Restrictions:")
+    display(instance.alliances)
+
+
+    # Initialize a trivial valid solution (each prisioner in its own prision)
+    function generateTrivialSolution()
+        solution = Vector{Prision}()
+        for i in 1:instance.n
+            push!(solution, [i])
+        end
+        return Solution(solution, instance.n)
+    end
+
+    # GRASP it
+    globalBest = generateTrivialSolution()
+    for i = 1:iterationsNum
+        initialSolution = randomGreedy(instance, 0.1)
+        solution = localSearch(initialSolution, instance)
+        if solution.value < globalBest.value
+            globalBest = deepcopy(solution)
+        end
+    end
+
+    println("\n\n")
+    println("######### END #########")
+    println("Solution is: $globalBest")
 end
 
-# GRASP it
-globalBest = generateTrivialSolution()
-for i = 1:1
-    initialSolution = randomGreedy(instance, 0.1)
-    solution = localSearch(initialSolution, instance)
-    if solution.value < globalBest.value
-        global globalBest = deepcopy(solution)
-    end
-end
-
-println("\n\n")
-println("######### END #########")
-println("Solution is: $globalBest")
+main()
