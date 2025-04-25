6d0eb60cc3da7e89e16947ac0a2522dc7fa4dd88
Guilherme Flach <gui.ismael.flach@gmail.com>
2025-04-25 18:47:33 -0300

    Added timing functions

--- main.jl
+++ main.jl
@@ -1,3 +1,4 @@
+import Dates
 include("src/reader.jl")
 include("src/instance.jl")
 include("src/randomGreedy.jl")
@@ -6,8 +7,21 @@ include("src/localSearch.jl")
 using Random
 
 function main()
+    programStartTime = Dates.now()
+
+
+    function displaySolution(solution::Solution, isFirstSolution=false)
+        elapsedTime = convert(Dates.DateTime, Dates.now() - programStartTime)
+        if (isFirstSolution)
+            println("Generated first solution at $(Dates.format(elapsedTime, "MM:SS.sss")) with value $(solution.value):")
+        else
+            println("Found better solution at $(Dates.format(elapsedTime, "MM:SS.sss")) with value $(solution.value):")
+        end
+        # display(solution.prisionsStructure)
+    end
+
     # Variables to be extracted as command line params
-    if (size(ARGS)[1] !== 3)
+    if (size(ARGS)[1] <= 3)
         println("Error reading args. Usage: `main.jl filepath iterations randomSeed`")
         return
     end
@@ -16,6 +30,16 @@ function main()
     iterationsNum = parse(Int, ARGS[2])
     randomSeed = parse(Int, ARGS[3])
 
+    alpha = 0.15
+    if (size(ARGS)[1] >= 4)
+        alpha = parse(Float64, ARGS[4])
+    end
+
+    showRestrictions = false
+    if (size(ARGS)[1] >= 5)
+        showRestrictions = parse(Bool, ARGS[5])
+    end
+
 
     Random.seed!(randomSeed)
 
@@ -23,9 +47,10 @@ function main()
 
     println("Number of prisioners: $(instance.n)")
     println("Number of restrictions: $(instance.m)")
-    println("Restrictions:")
-    display(instance.alliances)
-
+    if (showRestrictions)
+        println("Restrictions:")
+        display(instance.alliances)
+    end
 
     # Initialize a trivial valid solution (each prisioner in its own prision)
     function generateTrivialSolution()
@@ -37,19 +62,22 @@ function main()
     end
 
     # GRASP it
-    globalBest = randomGreedy(instance, 0.0) # Run a deterministic greedy for the base solution
-    println("Initialized base solution with value $(globalBest.value)")
+    globalBest = randomGreedy(instance, 1.0) # Run a deterministic greedy for the base solution
+    globalBest = localSearch(globalBest, instance)
+    displaySolution(globalBest, true)
     for i = 1:iterationsNum
-        initialSolution = randomGreedy(instance, 0.15)
-        solution = localSearch(initialSolution, instance)
+        println("GREEDY: $(@elapsed initialSolution = randomGreedy(instance, alpha))")
+        println("LOCAL SEARCH: $(@elapsed solution = localSearch(initialSolution, instance))")
+        println("######################")
         if solution.value < globalBest.value
             globalBest = deepcopy(solution)
-            println("Found better solution with value $(globalBest.value)")
+            displaySolution(globalBest)
         end
     end
 
-    println("\n\n")
+    println("\n")
     println("######### END #########")
+    println("Finished in: $(Dates.format(convert(Dates.DateTime, Dates.now() - programStartTime), "MM:SS.sss"))")
     println("Solution is: $globalBest")
 end
 
